62e709a428755bc2d6dee0ce51a44122b0623edf
“Your <totallyproemail@gmail.com>
2026-04-07 11:44:20 -0400

    Updated Welcome

--- TheChaseter.java
+++ TheChaseter.java
@@ -2,7 +2,7 @@
 public class TheChaseter {
 
 	public static void main(String[] args) {
-		System.out.println("welcom to my skibbity land!!");
+		System.out.println("welcome to my skibbity land!!");
 
 	}
 
